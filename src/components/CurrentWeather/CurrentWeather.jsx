1b59a281e3a1e424ba7199704f779fcafee376c1
Darle Gheorghe <darlegheorghe@yahoo.ro>
2022-01-18 11:13:07 +0200

    fix(current-weather): fix ui

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -4,20 +4,22 @@ import "./CurrentWeather.scss";
 export const CurrentWeather = () => {
   const weatherCode = "01d";
   return (
-    <>
+    <div className="container">
       <div className="current-weather">
-        <img
-          src={require(`../../resources/icon_${weatherCode}_n.png`)}
-          className="current-weather-image"
-          alt=""
-        />
-        <div>
-          <div className="current-weather-temp">23°</div>
-          <div className="current-weather-feelslike">Feels like: 27°</div>
-          <div className="current-weather-description">sunny</div>
+        <div className="image">
+          <img
+            src={require(`../../resources/icon_${weatherCode}_n.png`)}
+            className="icon"
+            alt=""
+          />
+        </div>
+        <div className="details">
+          <div className="temp">23°</div>
+          <div className="feelslike">Feels like: 27°</div>
+          <div className="description">sunny</div>
         </div>
       </div>
       <div className="current-weather-details"></div>
-    </>
+    </div>
   );
 };
